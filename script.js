b80db13f22dafbf153b77aa8cb66e1f7c4ee11be
DaviLadwig <ladwigdavi@gmail.com>
2026-03-16 16:03:19 -0300

    Seções hero e serviços concluidas.

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-document.getElementById("formAgendamento")
-    .addEventListener("submit", function (e) {
+const toggle = document.getElementById("menu-toggle");
+const nav = document.querySelector(".nav");
 
-        e.preventDefault();
-
-        let nome = document.getElementById("nome").value
-        let telefone = document.getElementById("telefone").value
-        let servico = document.getElementById("servico").value
-        let data = document.getElementById("data").value
-        let hora = document.getElementById("hora").value
-
-        let mensagem = `Olá, gostaria de confirmar um agendamento:
-
-Nome: ${nome}
-Telefone: ${telefone}
-Serviço: ${servico}
-Data: ${data}
-Hora: ${hora}`
-
-        let numero = "5599999999999"
-
-        let url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`
-
-        window.open(url, "_blank")
-
-    })
+toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
