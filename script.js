6224794e17c930172fabae3c88ad2eb0961db6db
DaviLadwig <ladwigdavi@gmail.com>
2026-03-19 00:33:44 -0300

    Site estilizado, faltando pequenos ajustes de responsividade.

--- script.js
+++ script.js
@@ -4,3 +4,36 @@ const nav = document.querySelector(".nav");
 toggle.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
+
+//CARROSSEL DE FEEDBACKS
+
+const track = document.querySelector(".carousel-track");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+let index = 0;
+
+function updateCarousel() {
+    const cardWidth = document.querySelector(".feedback-card").offsetWidth + 30;
+    track.style.transform = `translateX(-${index * cardWidth}px)`;
+}
+
+next.addEventListener("click", () => {
+    if (index < 2) {
+        index++;
+        updateCarousel();
+    }
+});
+
+prev.addEventListener("click", () => {
+    if (index > 0) {
+        index--;
+        updateCarousel();
+    }
+});
+
+/* autoplay */
+setInterval(() => {
+    index = (index + 1) % 3;
+    updateCarousel();
+}, 4000);
